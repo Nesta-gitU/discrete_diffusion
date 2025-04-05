a1b7e1d4db4856d564c891ac48f3424234f7fd46
Nesta-gitU <n.midavaine@student.vu.nl>
2025-04-05 12:35:54 +0000

    fix dynamic grad norming maybe

--- src/models/language_diff_module.py
+++ src/models/language_diff_module.py
@@ -244,6 +244,9 @@ class DiffusionModule(LightningModule):
                 self.current_grad_norm = alpha * self.current_grad_norm + (1 - alpha) * new_grad_norm
             else:
                 new_grad_norm = torch.nn.utils.clip_grad_norm_(self.parameters(), max_norm=self.current_grad_norm*1.5)
+                if new_grad_norm > self.current_grad_norm*1.5:
+                    print("we should have clipped")
+                    new_grad_norm = self.current_grad_norm*1.5 #add some downward bias to the gradient norm so it cannot blow up over the course of a couple steps 
                 self.log("current_clip", self.current_grad_norm*1.5, on_step=True, on_epoch=False, prog_bar=True, logger=True)
                 self.current_grad_norm = alpha * self.current_grad_norm + (1 - alpha) * new_grad_norm
             
