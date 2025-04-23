feb956cf24d944d2257bf87c4dd9fabd9a37055a
Nesta <nesta.midavaine@student.uva.nl>
2025-04-23 16:02:02 +0000

    fixes

--- src/models/ndm/ndm.py
+++ src/models/ndm/ndm.py
@@ -228,7 +228,7 @@ class NeuralDiffusion(nn.Module):
             if isinstance(self.context, NoneContext):
                 gamma, _ = self.gamma(t)
             else:
-                context = torch.zeros_like(x)
+                context = torch.zeros_like(x, dtype=x.dtype, device=x.device)
                 gamma = self.gamma.get_gamma(t, context)
 
             alpha = self.gamma.alpha_2(gamma) ** 0.5
