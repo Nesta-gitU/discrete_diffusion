76d84a1caa03a49929777e34c15f461ff715ec86
Nesta <nesta.midavaine@student.uva.nl>
2025-05-06 15:10:30 +0000

    100 bs

--- src/sample.py
+++ src/sample.py
@@ -335,7 +335,7 @@ def evaluate(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     elif args.setting == 'full_mode':
         args.std_split = 5
         args.num_samples = 5000
-        args.batch_size = 5000
+        args.batch_size = 1000
     elif args.setting == 'reference_mode':
         args.std_split = 5
         args.num_samples = 5000
