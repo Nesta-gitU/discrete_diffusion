1429b04e980879e714b046384cf47310b73f290d
Nesta <nesta.midavaine@student.uva.nl>
2025-05-07 19:32:36 +0000

    keep in memroy true

--- src/metrics/hf_ppl_under_ar.py
+++ src/metrics/hf_ppl_under_ar.py
@@ -4,6 +4,6 @@ from evaluate import load
 
 def compute_perplexity(all_texts_list, human_references, model_id='gpt2-large'):
     torch.cuda.empty_cache() 
-    perplexity = load("perplexity", module_type="metric")
+    perplexity = load("perplexity", module_type="metric", keep_in_memory=True)
     results = perplexity.compute(predictions=all_texts_list, model_id=model_id, device='cuda')
     return results['mean_perplexity']
