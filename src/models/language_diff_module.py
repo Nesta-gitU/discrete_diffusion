ac5693148b98750d60e9db381b5490570e9d5154
Nesta <nesta.midavaine@student.uva.nl>
2025-05-11 13:16:59 +0000

    req gtad is false

--- src/models/language_diff_module.py
+++ src/models/language_diff_module.py
@@ -102,7 +102,7 @@ class DiffusionModule(LightningModule):
         self.time_sampler = time_sampler
         #hack to make it so you can continue a training run with a different sampler
         if not hasattr(self.time_sampler, "_logits"):
-            self.time_sampler._logits = nn.Parameter(torch.ones(100))
+            self.time_sampler._logits = nn.Parameter(torch.ones(100), requires_grad=False)
         self.model = diffusion
         self.max_steps = total_steps
         self.mask_padding = mask_padding
